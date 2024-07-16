32bccf3c88c6f9642aa2602886d3797ad4cd3249
Yaswanth791504 <mallisettiyaswanth@gmail.com>
2024-07-16 21:40:35 +0530

    Reapply "third"
    This reverts commit cea85c805293cbd1ede53752e6cfb28342ae8b53.
    
    revet to third

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Provider } from "react-redux";
+import store from "./store/resumeStore";
+
 export default function Home() {
   return (
-    <main>
-      <div>Next Js App</div>
-    </main>
+    <Provider store={store}>
+      <main>
+        <div>Next Js App</div>
+      </main>
+    </Provider>
   );
 }
