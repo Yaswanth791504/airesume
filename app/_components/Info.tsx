fa693a4cd8172ad254a490354f3971a1be6b7ba7
Yaswanth791504 <mallisettiyaswanth@gmail.com>
2024-07-16 21:34:17 +0530

    Revert "second"
    This reverts commit 7deb316ea66dda163a67b88d76caf6d1dbe9426a.
    
    reverting

--- app/_components/Info.tsx
+++ app/_components/Info.tsx
@@ -1,30 +1,13 @@
 import { Outfit } from "next/font/google";
-import Experience from "./Experience";
-import Achievements from "./Achievements";
 
 const roboto = Outfit({ weight: "700", subsets: ["latin"] });
 
 export default function Info({ color }: { color: string }) {
   return (
-    <div className="h-fit w-full flex flex-col gap-3" style={{ color }}>
-      <div>
-        <h1
-          className={`text-xl tracking-wider lg:text-3xl ${roboto.className}`}
-        >
-          Yaswanth Mallisetti
-        </h1>
-        <h3 className="tracking-widest">Full Stack Developer</h3>
-      </div>
-      <div>
-        <p className="text-[9px] md:text-[10px] lg:text-[12px] text-black">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
-          repudiandae nesciunt quas amet fugiat quae velit, corporis est alias
-          nam ducimus quo? Sint ducimus, corrupti tempora reiciendis suscipit
-          esse sed.
-        </p>
-      </div>
-      <Experience color={color} />
-      <Achievements color={color} />
+    <div className="h-fit w-full" style={{ color }}>
+      <h1 className={`font-bold text-xl tracking-wider ${roboto.className}`}>
+        Yaswanth Mallisetti
+      </h1>
     </div>
   );
 }
--- app/resume/build/@editor/page.tsx
+++ app/resume/build/@editor/page.tsx
@@ -1,7 +1,3 @@
 export default function Page() {
-  return (
-    <div className="py-5 px-10 h-full gap-5 flex justify-center items-center">
-      Editor
-    </div>
-  );
+  return <div>Editor</div>;
 }
