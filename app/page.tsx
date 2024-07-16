cea85c805293cbd1ede53752e6cfb28342ae8b53
Yaswanth791504 <mallisettiyaswanth@gmail.com>
2024-07-16 21:32:52 +0530

    Revert "third"
    This reverts commit 9eec7c73b80d192aecf9d3ea3f025a72f3eec5cc.
    
    revert-to-previous

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { Provider } from "react-redux";
-import store from "./store/resumeStore";
-
 export default function Home() {
   return (
-    <Provider store={store}>
-      <main>
-        <div>Next Js App</div>
-      </main>
-    </Provider>
+    <main>
+      <div>Next Js App</div>
+    </main>
   );
 }
