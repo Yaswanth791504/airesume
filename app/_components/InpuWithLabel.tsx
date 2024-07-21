1ba0f3234cc11c3fadcb58b4be85e586baa2d3fa
Yaswanth791504 <mallisettiyaswanth@gmail.com>
2024-07-21 21:38:19 +0530

    input

--- app/_components/InpuWithLabel.tsx
+++
@@ -1,9 +0,0 @@
-export default function InputWithLabel({ register, inputName,errors }) {
-  return (
-    <div>
-      <label htmlFor={inputName}>{register}</label>
-      <input type="text" name={inputName} {...register(inputName)} />
-      {errors.inputName && }
-    </div>
-  );
-}
